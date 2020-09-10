6e74e7239048cba74b59f620ec24c763e30cc37b
WOOLUCK <wnsdn9504@gmail.com>
2020-09-10 10:58:38 +0900

    # Movie App 2020_09_10_2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Potato from './potato'; //추가 render를 위해서 불러옴 ./=same directory
 // import logo from './logo.svg'; 교육을 위해 지움
 // import './App.css'; 교육을 위해 지움
 
@@ -6,6 +7,9 @@ function App() {
   return (
     <div className="App">
       <h1>REACT LET'S GET IT</h1>
+      <Potato />
+      {/* Potato.js를 index.js에 바로 포함시킬 수도 있고 App.js에 포함할 수도 있음 */}
+     
       {/* 교육을 위해 지움
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 // import './index.css'; 일단 지움
 import App from './App';
 // import * as serviceWorker from './serviceWorker'; 일단 지움
+import Potato from './potato';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />  <Potato /> 
+    {/* <App /> = Component=html을 반환하는 함수 
+      JS와 HTML의 조합 = JSX (오직 React에만 존재)
+    */}
   </React.StrictMode>,
   document.getElementById('root')
 );
